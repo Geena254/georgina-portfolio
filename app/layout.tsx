cc1e6b7c2f0a74383ec9f519fa8021a6cbd411ae
Georgina Kimani <njokikimani001@gmail.com>
2025-04-13 02:36:48 +0300

    added icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,14 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "Georgina Kimani - Full-Stack Developer & UI/UX Designer",
+  icons: {
+    icon: "/images/favico.ico"
+  },
+  title: "GeorginaDev",
   description:
     "Portfolio of Georgina Kimani, a Full-Stack Developer and UI/UX Designer specializing in web technologies.",
   generator: "Next.js",
 }
-
 export default function RootLayout({
   children,
 }: {
